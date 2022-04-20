a8363218e9b3b2e0a2672c4731ac89efc2bb8580
Elaina-Alex <luanyin4444@qq.com>
2022-04-20 15:33:13 +0800

    优化epub规范

--- epub.py
+++ epub.py
@@ -27,32 +27,16 @@ class EpubFile:
         self.EpubList.append(intro_)
 
     def cover(self):
-        cover_jpg = BiquPavilionAPI.Cover.download_cover(Vars.book_info.cover_url)
+        cover_url = "https://imgapixs.pigqq.com/BookFiles/BookImages/" + Vars.book_info.cover_url
+        cover_jpg = BiquPavilionAPI.Cover.download_cover(cover_url)
         self.epub.set_cover(self.book_name + '.png', cover_jpg)
 
     def add_chapter(self, chapter_id: str, chapter_title: str, content: str, serial_number: str):
-        default_style = '''
-        body {font-size:100%;}
-        p{
-            font-family: Auto;
-            text-indent: 2em;
-        }
-        h1{
-            font-style: normal;
-            font-size: 20px;
-            font-family: Auto;
-        }      
-        '''
-        default_css = epub.EpubItem(uid="style_default", file_name="style/default.css", media_type="text/css",
-                                    content=default_style)
-
+        file_name = str(serial_number + 1).rjust(4, "0") + '-' + str(chapter_id) + '.xhtml'
         chapter_serial = epub.EpubHtml(
-            title=chapter_title, file_name=str(serial_number).rjust(4, "0") + '-' + str(chapter_id) + '.xhtml',
-            lang='zh-CN', uid='chapter_{}'.format(serial_number)
+            title=chapter_title, file_name=file_name, lang='zh-CN', uid='index-{}'.format(serial_number + 1)
         )
-
         chapter_serial.content = content.replace('\n', '</p>\r\n<p>')
-        chapter_serial.add_item(default_css)
         self.epub.add_item(chapter_serial)
         self.EpubList.append(chapter_serial)
 
@@ -63,32 +47,6 @@ class EpubFile:
         self.epub.spine.extend(self.EpubList)
         self.epub.add_item(epub.EpubNcx())
         self.epub.add_item(epub.EpubNav())
-        style = """
-                body {
-                    font-family: Auto;
-                }
-                p{
-                     font-family: Auto;
-                     text-indent: 2em;
-                }
-                h2 {
-                     text-align: left;
-                     text-transform: uppercase;
-                     font-weight: 200;     
-                }
-                ol {
-                        list-style-type: none;
-                }
-                ol > li:first-child {
-                        margin-top: 0.3em;
-                }
-                nav[epub|type~='toc'] > ol > li > ol  {
-                    list-style-type:square;
-                }
-                nav[epub|type~='toc'] > ol > li > ol > li {
-                        margin-top: 0.3em;
-                }"""
-        nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
-        self.epub.add_item(nav_css)
-        epub.write_epub(self.path(Vars.cfg.data.get('save_book'), self.book_name, self.book_name + '.epub'), self.epub,
-                        {})
+        epub.write_epub(
+            self.path(Vars.cfg.data.get('save_book'), self.book_name, self.book_name + '.epub'), self.epub, {}
+        )
